8a8d1de80f5dcc5007b8a9897b74340de98edf6f
66redBastard <66redbastard@gmail.com>
2021-09-30 07:10:30 +0300

    content-nav done, content data list - wip

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  buttons = [
+    'In Triage',
+    'Completed',
+    'Drafts',
+    'Rejected',
+    'Discharged',
+    'Archived',
+  ];
+  selectedItem: number = 0;
+  // buttons = [
+  //   {name:'In Triage', active: false},
+  //   {name:'Completed', active: false},
+  //   {name:'Drafts',  active: false},
+  //   {name:'Rejected',  active: true},
+  //   {name:'Discharged',  active: false},
+  //   {name:'Archived',  active: false}
+  // ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  // toggleClass(button) {
+  //   item.active = !item.active;
+  // }
+  ngOnInit(): void {}
 }
